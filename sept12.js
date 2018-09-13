0a95f4ce9c70c9de2be9c2af8bd964abdb42afa8
Veda <vedaadnani25@gmail.com>
2018-09-13 16:01:13 -0400

    circles

---
+++ sept12.js
@@ -0,0 +1,24 @@
+function setup() {
+    createCanvas(1000, 1000);
+	ellipseMode(CENTER);
+	
+}
+
+function draw(){
+     
+noStroke ();  
+fill(0, 150, 0);
+ellipse(500, 50, 20);
+
+
+noStroke ();  
+fill(150, 0, 0);
+ellipse(0, 0, 20);
+
+noStroke ();  
+fill(0, 0, 150);
+ellipse(800, 250, 80);
+
+
+    
+}
