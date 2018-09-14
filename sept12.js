3154a0d1ff8225bb2c7b6f5843ae510f1b812c8e
Veda <vedaadnani25@gmail.com>
2018-09-14 18:27:01 -0400

    notifications and settings placement and hover states with pop-ups

--- sept12.js
+++ sept12.js
@@ -1,24 +1,51 @@
+var canvas;
+
+
+
+
+
 function setup() {
-    createCanvas(1000, 1000);
-	ellipseMode(CENTER);
-	
+ canvas = createCanvas(900, 600);
+ canvas.position(300, 0);
+ ellipseMode(CENTER);
 }
 
+
 function draw(){
+	background (150);
+	clear();
+
+	
+stroke(255);
+line( 600,0, 800, 250);
+
      
 noStroke ();  
 fill(0, 150, 0);
 ellipse(500, 50, 20);
 
 
-noStroke ();  
 fill(150, 0, 0);
-ellipse(0, 0, 20);
+ellipse(600, 0, 20);
 
-noStroke ();  
 fill(0, 0, 150);
 ellipse(800, 250, 80);
+	
 
+fill(0, 100, 100);
+ellipse(400, 400, 60);
 
     
 }
+
+
+function showsecret(){
+document.getElementById("secret").style.display = "block";
+
+}
+
+function hidesecret(){
+document.getElementById("secret").style.display = "none";
+	
+	
+
---
+++ setp14.js
@@ -0,0 +1,13 @@
+var canvas;
+
+function setup(){
+canvas = createCanvas(400,400);	
+}
+
+            function showsecret(){
+                document.getElementById("secret").style.display = "block";
+            }
+
+            function hidesecret(){
+                document.getElementById("secret").style.display = "none";
+            }
